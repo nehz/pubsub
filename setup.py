6536613fa1a7ccfc00a4320437e605a93d6c21b2
Zhen Wang <mail@zhenwang.info>
2015-10-10 15:34:15 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='pubsub',
+    version='0.1.1',
+    description='Simple Python PubSub',
+    author='Zhen Wang',
+    author_email='ichigo.san@gmail.com',
+    url='https://bitbucket.org/nehz/pubsub',
+    py_modules=['pubsub'],
+)
